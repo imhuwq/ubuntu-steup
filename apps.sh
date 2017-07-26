fa3334fbab8d6b3e7159dbaf2a617f22567d01a1
imhuwq <imhuwq@gmail.com>
2017-07-26 19:24:17 +0800

    feat(apps): add more tools to beautify theme

--- apps.sh
+++ apps.sh
@@ -4,25 +4,29 @@ source helper/sscmd.sh
 source helper/vars.sh
 source helper/utils.sh
 
-mkdir -p download
-cd download
-
 call_seperator
-echo -e "${Red}Step 1${NC}: install chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpki -i google-chrome*.deb
+#echo -e "${Red}Step 1${NC}: install chrome"
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpki -i google-chrome*.deb
+#rm google-chrome*.deb
 
 call_seperator
 echo -e "${Red}Step 2${NC}: install terminator"
 sudo apt install terminator
+cp ./template/terminator/config ~/.config/terminator/config
+gsettings set org.gnome.desktop.default-applications.terminal exec 'terminator'
 
 call_seperator
 echo -e "${Red}Step 3${NC}: install screenfetch"
 sudo apt install screenfetch
 
 call_seperator
-echo -e "${Red}Step 4${NC}: install unity-tweak-tool"
+echo -e "${Red}Step 4${NC}: she walks in beauty"
 sudo apt install unity-tweak-tool
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-gtk-theme numix-icon-theme-circle
+sudo apt-get install compizconfig-settings-manager
 
 call_seperator
 echo -e "${Red}Step 5${NC}: install htop"

c8958b411cfbe2784e58626bb0f498f10f7e90f4
imhuwq <imhuwq@gmail.com>
2017-07-26 19:25:56 +0800

    fix(docker-ce): add missing step to add docker repositiry

--- docker-ce.sh
+++ docker-ce.sh
@@ -6,6 +6,7 @@ sudo apt remove -y docker docker-engine docker.io
 sudo apt  update
 sudo apt install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \

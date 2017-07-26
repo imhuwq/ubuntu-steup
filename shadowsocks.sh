ea448ba9f9f4b1d79876b0ae08c7a0fdb818439d
imhuwq <imhuwq@gmail.com>
2017-07-26 19:40:06 +0800

    feat(shadowsocks): install polipo for docker proxy

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -17,17 +17,24 @@ update_file /etc/privoxy/config 'forward-socks5 / 127.0.0.1:1080 .' sudo
 sudo service privoxy restart
 
 call_seperator
+echo -e "${Red}Step 3${NC}: install polipo"
+sudo apt install polipo
+sudo cp ./template/polipo/config /etc/polipo/config
+sudo service polipo restart
+
+call_seperator
 echo -e "${Red}Step 3${NC}: install proxychains"
 sudo apt install proxychains
 sudo sed -i -E "s/socks4\s+127.0.0.1\s+9050/# socks4 127.0.0.1 9050/" /etc/proxychains.conf
 update_file /etc/proxychains.conf 'socks5 127.0.0.1 1080' sudo
 
+call_seperator
+echo -e "${Red}Step 4${NC}: setup command alias"
+
 ss_begin='# setup http(s) proxy to shadowsocks'
 sscmd='alias sscmd="source $HOME/cmd/helper/sscmd.sh"'
 uscmd='alias uscmd="source $HOME/cmd/hel[er/uscmd.sh"'
 
-call_seperator
-echo -e "${Red}Step 4${NC}: setup command alias"
 grep -q "$ss_begin" $HOME/.bashrc || echo -e "\n$ss_begin\n$sscmd\n$uscmd\n" >> $HOME/.bashrc
 
 if [ -f $HOME/.zshrc ]; then
